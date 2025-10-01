292507150588da7148204bf696cf19deef589c9c
Eduardo Maia <eduardo.maia@sempreceub.com>
2025-10-01 09:46:04 -0300

    Update Texto.js

--- Texto.js
+++ Texto.js
@@ -19,7 +19,7 @@ const frases = /*Essas sao todas as curiosidades, pelo menos por enquanto*/
  "No brasil é proibido fabricar açucar em casa devido a uma lei criada durante o ciclo do açucar na historia ",
  "Os autores de Crônicas de Nárnia (Lewis) e Senhor dos Anéis (Tolkien) eram melhores amigos",
  "O nascimento da Cleopatra é mais próximo do lançamento do iphone que da construção das pirâmides",
- "Uma ave confundiu a careca de Ésquilo com uma pedra e do ar, soltou uma tartaruga que quando acertou a cabeça do grego, veio a mata-lo",
+ "Um grego chamado Ésquilo morreu pois uma aguia confundiu a careca dele com uma pedra, e do alto jogou uma tartaruga que matou o grego quando acertou sua cabeça",
  "A proclamação da republica foi motivada tambem pelo fato do Marechal Deodoro da Fonseca descobrir que sua esposa estava o traindo com o primeiro Ministro de Dom Pedro II",
  "O Quokka é considerado o animal mais feliz do mundo",
  "Analgesia Congênita é uma condição genética que torna uma pessoa imune a dor. Entretanto é rara e apenas 20 casos foram documentados na historia",
@@ -35,6 +35,22 @@ const frases = /*Essas sao todas as curiosidades, pelo menos por enquanto*/
  "O material mais leve do mundo é o aerogel de grafeno, que é sete vezes mais leve que o ar. O material é composto por 99,8% de ar e é tão forte quanto o aço",
  "O buraco mais profundo do mundo encontra-se na Russia e tem 12,2km de profundidade",
  "Genghis Khan foi um dos maiores imperadores do planeta, fundou o Império Mongol que conquistou cerca de 19 milhões de km²",
+ "O filme mais longo do mundo se chama Logistics e tem duração aproximada de 35 dias",
+ "Keanu Reeves doou 70% do salário de Matrix para pesquisas sobre Leucemia",
+ "As pegadas dos astronautas ainda estão na Lua",
+ "Nesse planeta ja nasceram cerca de 117 bilhões de humanos",
+ "Um homem chamado Tsutomu Yamaguchi foi a única pessoa a conseguir sobreviver aos ataques de Hiroshima e Nagazaki",
+ "A pessoa mais alta do mundo tinh a 2,72m e calçava algo em torno de 76",
+ "A maior árvore do Brasil tinha 88 metros, o equivalente a um prédio de 30 andares ou o a altura do Cristo Redentor sem o pedestal",
+ "Pra evitar que os jogadores tenham noção da passagem de tempo, Las Vegas evita colocar relogios para mante-los focados",
+ "A primeira aparição do homem aranha foi em Amazing Fantasy #15, o preço do gibi original pode chegar até 200 mil doláres",
+ "A primeira aparição do Superman foi em Action Comics #1, o valor do gibi original pode chegar até 6 milhões de doláres",
+ "Anatidaefobia é o medo constante de ser observado por patos",
+ "Cerca de 379 já morreram tentando tirar uma selfie",
+ "A mira dos cowboys era tão ruim que se eles quisessem acertar a cabeça de alguem, deveriam mirar no joelho",
+ "Um ser humano pode sobreviver no maximo 11 dias sem dormir",
+ "Salto alto originalmente era uma calçado masculino",
+ "A validade das garrafas de água na verdade indicam a validade da garrafa, pois depois de um tempo a garrafa libera substâncias toxicas na água."
 
 ];
 
